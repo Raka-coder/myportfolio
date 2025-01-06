01affd779ac1e25f13770a534d5b1c58c522ec43
Raka-coder <incudelta@gmail.com>
2025-01-06 23:20:11 +0700

    add scroll

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,14 +17,14 @@ const metadata = {
 function RootLayout({ children }) {
   
   return (
-    <html lang="en" className={poppins.className}> 
+    <html lang="en" className="!scroll-smooth"> 
     <head>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <meta name="description" content={metadata.description} />
       <link rel="icon" type="image/png" href="/favicon.ico" />
     </head>
-      <body className="">
+      <body className={poppins.className}>
         <LenisScroll>
           {children}
         </LenisScroll>
